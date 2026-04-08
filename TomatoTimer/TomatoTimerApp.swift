c4609e576ac48bd3d0e520cf475836a07a6497bb
Florian Knaus <florian.knaus@gmail.com>
2026-04-08 14:18:21 -0500

    Initial Commit

---
+++ TomatoTimer/TomatoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimerApp.swift
+//  TomatoTimer
+//
+//  Created by Florian Knaus on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TomatoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
